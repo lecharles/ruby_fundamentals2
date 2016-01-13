7b06ba6dbeed32faa4fa73b89cff13f5a733c67e
lecharles <charleslozano@gmail.com>
2016-01-13 16:54:15 -0500

    Check to see if includes or not a missing_item and outputs message accordingly

--- exercise6-1.rb
+++ exercise6-1.rb
@@ -5,12 +5,22 @@
 @list = ["carrots", "rolling paper", "bananas", "apples"]
 #lista_de_compras.each { |item_on_the_list| puts "* " + item_on_the_list }
 
-def grocery_list (item_to_list = nil)
+def grocery_list (item_to_list = nil, missing_item)
+  #Adding new items to the list unless user does not want to add new item
   @list << item_to_list unless item_to_list == nil
+
+  #Checking if you're missing an item
+  if @list.include?(missing_item)
+    puts "You're good on #{missing_item}"
+  else
+    puts "You're low on #{missing_item}. Get some!"
+  end
+
+  #Counting the number of items on the list
   number_of_items = @list.count
   puts "Total number of items to purchase is #{number_of_items}, your list is:"
   puts #space for better reading
   @list.each { |item_to_list| puts "* " + item_to_list }
 end
 
-grocery_list("piña")
+grocery_list("piña", "bananas")
