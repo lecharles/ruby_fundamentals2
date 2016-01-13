3f3586132c6436b1408ecc9b523bfd53d6ea9292
lecharles <charleslozano@gmail.com>
2016-01-13 15:34:51 -0500

    Add rice (and any item) to the list via a reusable method.

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,13 @@
 # def showArray
 #   lista_de_compras = ["carrots", "rolling paper", "bananas", "apples"]
 # end
+
 lista_de_compras = ["carrots", "rolling paper", "bananas", "apples"]
-lista_de_compras.each { |item_on_the_list| puts "* " + item_on_the_list }
+#lista_de_compras.each { |item_on_the_list| puts "* " + item_on_the_list }
+
+def grocery_list (list, item_to_list)
+  list << item_to_list
+  list.each { |item_to_list| puts "* " + item_to_list }
+end
+
+grocery_list(lista_de_compras, "rice")
