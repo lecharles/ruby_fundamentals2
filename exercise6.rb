371b295b8282380083a33b78fa54e7ef7178a95e
lecharles <charleslozano@gmail.com>
2016-01-13 15:06:56 -0500

    Present your grocery list with an item on each line, with an asterisk (*) in front of it

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,5 @@
-def showArray
-  lista_de_compras = ["carrots", "rolling paper", "bananas", "apples"]
-end
-puts showArray
+# def showArray
+#   lista_de_compras = ["carrots", "rolling paper", "bananas", "apples"]
+# end
+lista_de_compras = ["carrots", "rolling paper", "bananas", "apples"]
+lista_de_compras.each { |item_on_the_list| puts "* " + item_on_the_list }
