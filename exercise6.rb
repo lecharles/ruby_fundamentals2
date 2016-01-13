d83455b95062db8e58a4ec6b0b30dec9965381aa
lecharles <charleslozano@gmail.com>
2016-01-13 16:15:07 -0500

    2. Count how many things you need to pick up. Better output the total number of items on your list.

--- exercise6.rb
+++ exercise6.rb
@@ -7,7 +7,12 @@ lista_de_compras = ["carrots", "rolling paper", "bananas", "apples"]
 
 def grocery_list (list, item_to_list)
   list << item_to_list
+  number_of_items = list.count
+  puts "Total number of items to purchase is #{number_of_items}, your list is:"
+  puts #space for better reading
   list.each { |item_to_list| puts "* " + item_to_list }
+
+
 end
 
 grocery_list(lista_de_compras, "rice")
