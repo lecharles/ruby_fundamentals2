60566b7cf353c86f550972a7d4e9cb2c3156f1be
lecharles <charleslozano@gmail.com>
2016-01-13 17:13:09 -0500

    3. Check to see if list includes a specific item or not and output message accordingly. Create a Method per operation: 3 methods. 3 calls.

--- exercise6-1.rb
+++ exercise6-1.rb
@@ -1,26 +1,32 @@
-# def showArray
-#   lista_de_compras = ["carrots", "rolling paper", "bananas", "apples"]
-# end
-
+#DEFINING THE ORGINAL GROCERY LIST
 @list = ["carrots", "rolling paper", "bananas", "apples"]
-#lista_de_compras.each { |item_on_the_list| puts "* " + item_on_the_list }
 
-def grocery_list (item_to_list = nil, missing_item)
+#ADDING NEW ITEMS ON THE LIST
+def grocery_list (item_to_list = nil)
   #Adding new items to the list unless user does not want to add new item
   @list << item_to_list unless item_to_list == nil
 
-  #Checking if you're missing an item
-  if @list.include?(missing_item)
-    puts "You're good on #{missing_item}"
-  else
-    puts "You're low on #{missing_item}. Get some!"
-  end
-
-  #Counting the number of items on the list
-  number_of_items = @list.count
-  puts "Total number of items to purchase is #{number_of_items}, your list is:"
-  puts #space for better reading
+  #Printing the list line by line including * item name
   @list.each { |item_to_list| puts "* " + item_to_list }
 end
 
-grocery_list("piña", "bananas")
+#COUTING NUMBER OF ITEMS ON THE LIST
+def count_number_items (list)
+  puts #a space for better reading
+  number_of_items = @list.count
+  puts "Total number of items to purchase is #{number_of_items}."
+end
+
+#CHECKING IF MISSING ITEM ON THE LIST
+def check_if_missing_item (item)
+  puts #a space for better reading
+  if @list.include?(item)
+    puts "You're good on #{item} & you're good to go."
+  else
+    puts "You're low on #{item}. Get some!"
+  end
+end
+
+grocery_list("piña")
+count_number_items(@list)
+check_if_missing_item("aguacate")
