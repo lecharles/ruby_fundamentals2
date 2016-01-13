aad7465060724dc9ba33e1be688f59ead4f604bf
lecharles <charleslozano@gmail.com>
2016-01-13 17:46:37 -0500

    Delete item and redisplay the list.

--- exercise6-1.rb
+++ exercise6-1.rb
@@ -1,13 +1,16 @@
 #DEFINING THE ORGINAL GROCERY LIST
 @list = ["carrots", "rolling paper", "bananas", "apples"]
 
+#PRINTING THE LIST
+def print_list_to_screen (list = nil)
+  #Printing the list line by line including * item name
+  @list.each { |item| puts "* " + item }
+end
+
 #ADDING NEW ITEMS ON THE LIST
 def grocery_list (item_to_list = nil)
   #Adding new items to the list unless user does not want to add new item
   @list << item_to_list unless item_to_list == nil
-
-  #Printing the list line by line including * item name
-  @list.each { |item_to_list| puts "* " + item_to_list }
 end
 
 #COUTING NUMBER OF ITEMS ON THE LIST
@@ -27,15 +30,26 @@ def check_if_missing_item (item)
   end
 end
 
-grocery_list("piña")
-count_number_items(@list)
-check_if_missing_item("aguacate")
-
 puts #a space-line for better reading
 
-puts @list[1]
-
-puts #a space-line for better reading
-
+# grocery_list("piña")
+# count_number_items(@list)
+# check_if_missing_item("aguacate")
+#
+# puts #a space-line for better reading
+#
+# puts @list[1]
+#
+# puts #a space-line for better reading
+#
 @list = @list.sort
-grocery_list
+#grocery_list
+
+grocery_list("salmon")
+@list = @list.sort
+print_list_to_screen
+
+puts #a space-line for better reading
+
+@list.pop
+print_list_to_screen
