85249cb92362239d8a3e1ccfe58b035b27f1453f
lecharles <charleslozano@gmail.com>
2016-01-13 14:30:10 -0500

    Exercise 5 now with the assigment formula and indication about outputing full sentence

--- exercise5.rb
+++ exercise5.rb
@@ -1,9 +1,9 @@
 def fahrenheit_to_celcius (temperature_in_F)
-  temperature_in_C = (temperature_in_F - 32) * 0.556
+  temperature_in_C = (temperature_in_F - 32) * (5/9)
   temperature_in_C
 end
 
 puts "Dear User, please type what ºF temperature to convert in ºC:"
 temperature_in_F = gets.chomp
 temperature_in_C = fahrenheit_to_celcius(temperature_in_F.to_i)
-puts temperature_in_C
+puts "La température en degré celcius est de #{temperature_in_C}º"
